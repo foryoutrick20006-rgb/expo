1b1588a9ce50d91d46380cb00f37330c28ed7089
Hassan Khan <hassan@expo.dev>
2025-10-15 14:32:26 +0100

    refactor(@expo/router-server): move server code from `expo-router`

--- packages/@expo/cli/e2e/__tests__/customize-test.ts
+++ packages/@expo/cli/e2e/__tests__/customize-test.ts
@@ -90,6 +90,8 @@ it('runs `npx expo customize tsconfig.json`', async () => {
     {
       reuseExisting: false,
       sdkVersion: '52.0.0',
+      // TODO(@hassankhan): remove @expo/router-server after publishing
+      linkExpoPackages: ['@expo/router-server'],
     }
   );
 
@@ -111,6 +113,8 @@ it('runs `npx expo customize tsconfig.json` on a partially setup project', async
     {
       reuseExisting: false,
       sdkVersion: '52.0.0',
+      // TODO(@hassankhan): remove @expo/router-server after publishing
+      linkExpoPackages: ['@expo/router-server'],
     }
   );
 
@@ -144,7 +148,11 @@ it('runs `npx expo customize tsconfig.json` sets up typed routes', async () => {
   const projectRoot = await setupTestProjectWithOptionsAsync(
     'expo-customize-typed-routes',
     'with-router-typed-routes',
-    { reuseExisting: false, linkExpoPackages: ['expo-router'] }
+    {
+      reuseExisting: false,
+      // TODO(@hassankhan): remove @expo/router-server after publishing
+      linkExpoPackages: ['expo-router', '@expo/router-server'],
+    }
   );
 
   // `npx expo customize tsconfig.json`
